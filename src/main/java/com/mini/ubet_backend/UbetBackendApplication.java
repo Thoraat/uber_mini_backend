8cc2aa59e6cb61a3134f4690c7c85034f435a731
Amosh singh <amoshrana4thansingh@gmail.com>
2026-06-01 12:53:41 +0530

    integrated postgres using docker

---
+++ src/main/java/com/mini/ubet_backend/UbetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mini.ubet_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class UbetBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UbetBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mini/ubet_backend/UbetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mini.ubet_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
