f74e179519f5581f007c009a51c487acccb9a0ad
Amosh singh <amoshrana4thansingh@gmail.com>
2026-06-01 13:17:18 +0530

    removed hardcoded values

---
+++ src/main/java/com/mini/ubet_backend/Controller/HealthCheck.java
@@ -0,0 +1,13 @@
+package com.mini.ubet_backend.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheck {
+
+    @GetMapping("/health")
+    public String healthCheck(){
+        return "running fine";
+    }
+}
